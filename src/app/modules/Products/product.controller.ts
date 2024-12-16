43a729d2c02ebd66a6433f8279fdd08a7f1e5cb1
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2024-12-16 15:43:34 +0600

    another update

--- src/app/modules/Products/product.controller.ts
+++ src/app/modules/Products/product.controller.ts
@@ -4,6 +4,8 @@ import sendResponse from '../../utils/sendResponse';
 import { Request, RequestHandler, Response } from 'express';
 import { ProdcutServices } from './product.service';
 import { User } from '../User/user.model';
+import ip from 'ip';
+import requestIp from 'request-ip';
 
 const createProduct = catchAsync(async (req: Request, res: Response) => {
   const result = await ProdcutServices.createProductIntoDB(req);
@@ -20,16 +22,21 @@ const getAllProduct = catchAsync(async (req: Request, res: Response) => {
   // console.log(req.socket.remoteAddress, 'req');
   // console.log(req.headers['user-agent']);
   // const ip = req.headers['x-forwarded-for'];
-  // console.log(ip, 'ip');
-  const ip =
-    req.socket.remoteAddress === '::1' ? req.socket.remoteAddress : '127.0.0.1';
-  console.log(ip, 'req');
+
   const token = req.headers.authorization || '';
   const base64Url = token.split('.')[1];
   const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
   const payload = JSON.parse(Buffer.from(base64, 'base64').toString('utf-8'));
-  //  / console.log(payload, 'apyload');
-  console.log(req);
+
+  let ip = req.socket.remoteAddress || '';
+  if (ip.startsWith('::ffff:')) {
+    ip = ip.substring(7); // IPV4 address
+  }
+  console.log(ip, 'ip');
+
+  let ips = requestIp.getClientIp(req);
+  console.log(ips, 'ips');
+
   await User.updateOne({ email: payload?.email }, { ipAddress: ip });
 
   const result = await ProdcutServices.getProductIntoDB();
