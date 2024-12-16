2f264aff71abae667ad99497f69d60b5a7760597
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2024-12-16 16:03:39 +0600

    another update

--- src/app/modules/Products/product.controller.ts
+++ src/app/modules/Products/product.controller.ts
@@ -19,26 +19,6 @@ const createProduct = catchAsync(async (req: Request, res: Response) => {
 });
 
 const getAllProduct = catchAsync(async (req: Request, res: Response) => {
-  // console.log(req.socket.remoteAddress, 'req');
-  // console.log(req.headers['user-agent']);
-  // const ip = req.headers['x-forwarded-for'];
-
-  const token = req.headers.authorization || '';
-  const base64Url = token.split('.')[1];
-  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  const payload = JSON.parse(Buffer.from(base64, 'base64').toString('utf-8'));
-
-  let ip = req.socket.remoteAddress || '';
-  if (ip.startsWith('::ffff:')) {
-    ip = ip.substring(7); // IPV4 address
-  }
-  console.log(ip, 'ipp');
-
-  let ips = requestIp.getClientIp(req);
-  console.log(ips, 'ips');
-
-  await User.updateOne({ email: payload?.email }, { ipAddress: ip });
-
   const result = await ProdcutServices.getProductIntoDB();
 
   sendResponse(res, {
